7fafe955c3cf6c49d4e85ee1a82c30bc732192ed
kuki <ubarcturus@gmail.com>
2020-04-18 19:38:20 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function MyWidget(props: { text: string }) {
+  return <div>{props.text}</div>;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <MyWidget text="foobar" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
